8fda4b9f7269b6637c7726d6775d637c7e83c420
Imdad15 <imdad92@live.com>
2020-06-14 18:29:50 +0800

    add oracleRequest and flightstatus

--- src/server/server.js
+++ src/server/server.js
@@ -7,63 +7,38 @@ const config = Config['localhost'];
 const web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
 web3.eth.defaultAccount = web3.eth.accounts[0];
 const flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
-const oracleCount = 20;
+const oraclesCount = 20;
 const oracles = [];
 const STATUS_CODES = [0, 10, 20, 30, 40, 50];
 
-async function setup(){
-  const accounts = await web3.eth.getAccounts();
-  registerOracles(accounts.slice(1, oracleCount + 1));
-
-  flightSuretyApp.events.OracleRequest({ fromBlock: 0 }, function (error, event) {
-    console.log(`evetn-  ${JSON.stringify(event)}`)
-    if (error) {
-      return console.log(error);
-    }
-    if (!event.returnValues) {
-      return console.error("No return values");
-    }
-    
-    const { returnValues } = event;
-  
-    fetchFlightStatus( returnValues.index, returnValues.airline,
-                            returnValues.flight, returnValues.timestamp);
-  });
-}
-
-async function registerOracles(account){
-  // ARRANGE
-  const fee = await config.flightSuretyApp.REGISTRATION_FEE.call();
-
-  for(let i = 0; i < oracleCount; i++) {
-    await config.flightSuretyApp.registerOracle({ from: accounts[i], value: fee });
-    const indexes = await config.flightSuretyApp.getMyIndexes.call({from: accounts[i]});
-    //add random flight status to oracle
-    const statusCode = STATUS_CODES[Math.floor(Math.random() * STATUS_CODES.length)];
-    oracles.push({address, indexes, statusCode});
-
-    console.log(`Oracle: ${address} indexes: ${result[0]}, ${result[1]}, ${result[2]}
-        statusCode: ${statusCode}`);
+web3.eth.getAccounts((error, accounts) => {
+  for(let a = 0; a < oraclesCount; a++) {
+    flightSuretyApp.methods.registerOracle().send({from: accounts[a], value: web3.utils.toWei("1",'ether'), gas: 9999999}, (error, result) => {
+      flightSuretyApp.methods.getMyIndexes().call({from: accounts[a]}, (error, indexes) => {
+        const statusCode = STATUS_CODES[Math.floor(Math.random() * STATUS_CODES.length)];
+        console.log(`indexes- ${JSON.stringify(indexes)}`)
+        const oracle = {address: accounts[a], indexes: indexes, statusCode: statusCode};
+        oracles.push(oracle);
+        console.log("ORACLE REGISTERED: " + JSON.stringify(oracle));
+      });
+    });
   }
-}
+});
 
-async function fetchFlightStatus(index, airline, flight, timestamp){
-  console.log(`Fetch flight status for - `);
-  console.log(index, airline, flight, timestamp);
+flightSuretyApp.events.OracleRequest({fromBlock: 0}, function (error, event) {
+  const {returnValues} = event;
+    let index = returnValues.index;
+    for(let a = 0; a < oracles.length; a++) {
+      console.log(`oracleReq- ${JSON.stringify(oracles[a])}`);
 
-  const indexMatchingOracles = [];
-
-  oracles.forEach((oracle) => {
-    if(oracle.indexes.includes(index)){
-      flightSuretyApp.methods.submitOracleResponse(index, airline, flight, timestamp, oracle.statusCode)
-      .send({ from: oracle.address, gas: 5555555 })
-      .then(() => {
-          console.log("Oracle responded with " + oracle.statusCode);
-      })
-      .catch((err) => console.log("Oracle response rejected"));
+      // if(oracles[a].indexes.includes(index)) {
+      //   flightSuretyApp.methods.submitOracleResponse(index, returnValues.airline, returnValues.flight, returnValues.timestamp, oracles[a].statusCode)
+      //   .send({from: oracles[a].address, gas: 9999999}, (error, result) => {
+      //     console.log("FROM " + JSON.stringify(oracles[a]) + "STATUS CODE: " + statusCode);
+      //   });
+      // }
     }
-  });
-}
+});
 
 const app = express();
 app.get('/api', (req, res) => {
@@ -71,9 +46,6 @@ app.get('/api', (req, res) => {
       message: 'An API for use with Dapp!'
     })
 })
-
-setup();
-
 
 export default app;
 
